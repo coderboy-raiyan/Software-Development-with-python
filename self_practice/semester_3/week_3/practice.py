0d8bea39976500719071a5d09cb23337d0ecea94
Raiyan <tajkierhaque@gmail.com>
2023-09-27 16:28:37 +0600

    practice done

---
+++ self_practice/semester_3/week_3/practice.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+
+txt = "one one was a race horse, two two was one too."
+
+x = txt.replace("one", "three")
+strList = list(x)
+
+strTuple = "Raiyan", "Turno", False
+strList = list(strTuple)
+
+print(strList)
